5560ce3fc105b0babde567b39b8728b944afd407
Drew Carrillo <drewc3@gmail.com>
2019-11-24 13:25:02 -0700

    ready for production

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const POKEDEX = require('./pokedex.json')
+const cors = require('cors')
+const helmet = require('helmet')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+
+app.use(helmet())
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
@@ -45,8 +51,16 @@ app.get('/pokemon', function handleGetPokemon(req, res) {
   res.json(response)
 })
 
-const PORT = 8000
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
 })
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT)
